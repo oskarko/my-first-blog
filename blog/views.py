a7719842c254e086fcba066163f35125bb6b3c43
Óscar Rodríguez <orodriguez@soax.es>
2016-05-25 16:38:08 +0200

    CSS corregido.

--- blog/views.py
+++ blog/views.py
@@ -71,6 +71,7 @@ def post_remove(request, pk):
     return redirect('blog.views.post_list')
 
 
+@login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
