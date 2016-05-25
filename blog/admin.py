d24a917b30719722f6412945b2e194790aafc6fb
Óscar Rodríguez <orodriguez@soax.es>
2016-05-25 15:28:47 +0200

    implementada sección de comentarios en cada post.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment  # importa de blog/models.py
 
+# tablas de la BD a las que accederemos desde el panel de admin
 admin.site.register(Post)
+admin.site.register(Comment)
